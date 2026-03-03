83861cf89c29097e4faf37423635ba13706fd284
Mansi Mehra <mansi2004mehra18@gmail.com>
2026-03-03 22:41:35 +0530

    update

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
